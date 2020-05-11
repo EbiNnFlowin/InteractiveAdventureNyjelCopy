16b4334ac0847f354623bcee7d2d0031d891f676
CryoFlar3 <nyjel.dukes@gmail.com>
2020-05-11 17:39:32 -0400

    Set up Story, Page and Choice class.

--- app/src/main/java/com/example/chooseyourownadventure/ui/StoryActivity.java
+++ app/src/main/java/com/example/chooseyourownadventure/ui/StoryActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.example.chooseyourownadventure.R;
+import com.example.chooseyourownadventure.model.Page;
 
 public class StoryActivity extends AppCompatActivity {
 
