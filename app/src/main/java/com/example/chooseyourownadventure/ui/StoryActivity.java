f8542b4963ebc45997e3e05894758ee37de1a3c8
ebintobin <55466612+ebintobin@users.noreply.github.com>
2020-05-18 17:49:02 -0400

    Almost done

--- app/src/main/java/com/example/chooseyourownadventure/ui/StoryActivity.java
+++ app/src/main/java/com/example/chooseyourownadventure/ui/StoryActivity.java
@@ -1,29 +1,59 @@
 package com.example.chooseyourownadventure.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.chooseyourownadventure.R;
 import com.example.chooseyourownadventure.model.Page;
+import com.example.chooseyourownadventure.model.Story;
 
 public class StoryActivity extends AppCompatActivity {
 
     private static final String TAG = StoryActivity.class.getSimpleName();
+    private Story story;
+    private ImageView storyImageView;
+    private TextView storyTextView;
+    private Button choice1Button;
+    private Button choice2Button;
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+        storyImageView = findViewById(R.id.storyImageView);
+        storyTextView = findViewById(R.id.storyTextView);
+        choice1Button = findViewById(R.id.choice1Button);
+        choice2Button = findViewById(R.id.choice2Button);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
+        name = intent.getStringExtra(getString(R.string.key_name));
         if (name == null || name.isEmpty()){
             name = "Player";
         }
 
         Log.d(TAG, name);
+        story = new Story();
+
+        loadPage(0);
+    }
+
+    private void loadPage(int pageNumber) {
+        Page page = story.getPage(pageNumber);
+        Drawable image = ContextCompat.getDrawable(this,page.getImageId());
+        storyImageView.setImageDrawable(image);
+
+        String pageText = getString(page.getTextId());
+
+        pageText = String.format(pageText, name);
+        storyTextView.setText(pageText);
     }
 }
