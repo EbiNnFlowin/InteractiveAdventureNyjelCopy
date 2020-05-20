baa37feecddb81d9f169a7a44603e28f684a65d5
ebintobin <55466612+ebintobin@users.noreply.github.com>
2020-05-20 17:09:59 -0400

    Finished App

--- app/src/main/java/com/example/chooseyourownadventure/ui/StoryActivity.java
+++ app/src/main/java/com/example/chooseyourownadventure/ui/StoryActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,6 +15,8 @@ import android.widget.TextView;
 import com.example.chooseyourownadventure.R;
 import com.example.chooseyourownadventure.model.Page;
 import com.example.chooseyourownadventure.model.Story;
+
+import java.util.Stack;
 
 public class StoryActivity extends AppCompatActivity {
 
@@ -24,6 +27,7 @@ public class StoryActivity extends AppCompatActivity {
     private Button choice1Button;
     private Button choice2Button;
     private String name;
+    private Stack<Integer> pageStack = new Stack<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +40,7 @@ public class StoryActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         name = intent.getStringExtra(getString(R.string.key_name));
-        if (name == null || name.isEmpty()){
+        if (name == null || name.isEmpty()) {
             name = "Player";
         }
 
@@ -47,13 +51,62 @@ public class StoryActivity extends AppCompatActivity {
     }
 
     private void loadPage(int pageNumber) {
+        pageStack.push(pageNumber);
         Page page = story.getPage(pageNumber);
-        Drawable image = ContextCompat.getDrawable(this,page.getImageId());
+        Drawable image = ContextCompat.getDrawable(this, page.getImageId());
         storyImageView.setImageDrawable(image);
 
         String pageText = getString(page.getTextId());
 
         pageText = String.format(pageText, name);
         storyTextView.setText(pageText);
+
+        if (page.isFinalPage()) {
+            choice1Button.setVisibility(View.INVISIBLE);
+            choice2Button.setText(R.string.play_again_button_text);
+            choice2Button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    loadPage(0);
+                }
+            });
+        } else {
+            loadButtons(page);
+        }
     }
+
+    private void loadButtons(final Page page) {
+        choice1Button.setVisibility(View.VISIBLE);
+        choice1Button.setText(page.getChoice1().getTextId());
+        choice1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int nextPage = page.getChoice1().getNextPage();
+                loadPage(nextPage);
+            }
+        });
+
+        choice2Button.setVisibility(View.VISIBLE);
+        choice2Button.setText(page.getChoice2().getTextId());
+        choice2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int nextPage = page.getChoice2().getNextPage();
+                loadPage(nextPage);
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        pageStack.pop();
+        if (pageStack.isEmpty()) {
+            super.onBackPressed();
+        } else {
+            loadPage(pageStack.pop());
+        }
+
+    }
+
 }
